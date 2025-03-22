325be17b6cf145827e2b72e977318866367ba71e
Muhammad Haikal <75706383+itshaiheree@users.noreply.github.com>
2025-03-22 07:56:53 +0700

    aaaaa

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ sock.ev.on('connection.update', async (update) => {
   // In prod, send this string to your frontend then generate the QR there
   if (qr) {
     // as an example, this prints the qr code to the terminal
-    console.log(await QRCode.toString(qr, {type:'terminal'}))
+    console.log(QRCode.toString(qr, {type:'terminal'}))
   }
 })
 
 // DO NOT USE IN PROD!!!!
-const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
+const { state, saveCreds } = useMultiFileAuthState("auth_info_baileys");
 // this will be called as soon as the credentials are updated
 sock.ev.on("creds.update", saveCreds);
 
@@ -78,55 +78,6 @@ res.send(`Oopsie! Requestmu ditahan dikarenakan kesalahan sistem!\nKontak owner 
 console.log(e)
 }
 })
-
-
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = '<https://jujurly.mhai.my.id/auth/google/callback>';
-
-// Initiates the Google Login flow
-router.get('/auth/google', (req, res) => {
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=profile email`;
-  res.redirect(url);
-});
-
-// Callback URL for handling the Google Login response
-router.get('/auth/google/callback', async (req, res) => {
-  const { code } = req.query;
-
-  try {
-    // Exchange authorization code for access token
-    const { data } = await axios.post('<https://oauth2.googleapis.com/token>', {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      code,
-      redirect_uri: REDIRECT_URI,
-      grant_type: 'authorization_code',
-    });
-
-    const { access_token, id_token } = data;
-
-    // Use access_token or id_token to fetch user profile
-    const { data: profile } = await axios.get('<https://www.googleapis.com/oauth2/v1/userinfo>', {
-      headers: { Authorization: `Bearer ${access_token}` },
-    });
-
-    // Code to handle user authentication and retrieval using the profile data
-
-    res.redirect('/');
-  } catch (error) {
-    console.error('Error:', error.response.data.error);
-    res.redirect('/login');
-  }
-});
-
-// Logout route
-router.get('/logout', (req, res) => {
-  // Code to handle user logout
-  res.redirect('/login');
-});
-
-
 
 
 
