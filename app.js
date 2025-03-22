93826ef6040e749358c4f8b6c25f28ae90307913
Muhammad Haikal <king.haikalganteng@gmail.com>
2025-03-22 15:19:56 +0700

    pepeq

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 var express = require("express");
 const fetch = require('node-fetch');
 var app = express();
+
+// we've started you off with Express,
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/index.html");
 });
-
-app.use(
-  (request, response, next) => {
-    response.set('X-Frame-Options', 'deny');
-    next();
-  },
-  express.static('public')
-);
 
 //  Response 404 Page
 app.use(function(req, res, next){
@@ -21,6 +17,15 @@ app.use(function(req, res, next){
     return;
   }
 });
+
+
+app.use(
+  (request, response, next) => {
+    response.set('X-Frame-Options', 'deny');
+    next();
+  },
+  express.static('public')
+);
 
 // listen for requests :)
 var listener = app.listen(1949, function() {
