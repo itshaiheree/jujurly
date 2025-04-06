f5b3bbadfdd222c45f162b613943de444223fd27
Muhammad Haikal <king.haikalganteng@gmail.com>
2025-04-06 16:07:11 +0700

    lets test this one

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const path = require("path");
 const mongoose = require('mongoose');
 mongoose.connect(`mongodb://${process.env.dbUsername}:${process.env.dbPassword}@${process.env.dbLink}?retryWrites=true&w=majority`);
 
+
+// function database
 const UsersSchema = new mongoose.Schema(
   {
     id: { type: String, required: true },
@@ -17,7 +19,15 @@ const UsersSchema = new mongoose.Schema(
   { versionKey: false, timestamps: true }
 );
 
+const noteCountSchema = new mongoose.Schema(
+  {
+    current: { type: String, required: true }
+    next: { type: String, required: true }
+  }
+);
+
 let Note = mongoose.model("Note", UsersSchema);
+let NoteCount = mongoose.model("NoteCount", noteCountSchema);
 
 async function addNote(userId, sender, target, pesan, music, link) {
   let obj = {
@@ -31,8 +41,39 @@ async function addNote(userId, sender, target, pesan, music, link) {
   Note.create(obj);
 }
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+async function setCurrentNote() {
+  const nextNumber = NoteCount.find().current + 1;
+
+  if (nextNumber === "22" || nextNumber === "24") {
+    const nextNumberr = NoteCount.find().current + 2;
+
+    await NoteCount.find().updateMany({
+      current: data.current,
+      next: nextNumberr
+    });
+  } else {
+    const nextNumberr = NoteCount.find().current + 1;
+
+    await NoteCount.find().updateMany({
+      current: data.current,
+      next: nextNumberr
+    });
+  }
+
+
+}
+
+async function setFirstNote(number) {
+  if (!number) { return false }
+  const nextNumber = number + 1
+  
+  let obj = {
+    current: number,
+    next: nextNumber
+  };
+
+  NoteCount.create(obj);
+}
 
 // homepage
 app.get("/", function(request, response) {
@@ -41,14 +82,14 @@ app.get("/", function(request, response) {
 
 <html data-theme="light" lang="en">
   <head>
-    <link rel="icon" href="./logo.png" type="image/x-icon" />
+    <link rel="icon" href="/logo.png" type="image/x-icon" />
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="sendanote" content="true" />
 
     <title>Send a Note - SEND YOUR NOTES!</title>
 
-    <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@2.15.3/dist/full.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
@@ -191,16 +232,183 @@ app.get("/", function(request, response) {
   `)
 });
 
-app.get("/notes/new", function(req, res) {
+// DELETE THIS PLS
+app.get("/notes/setid", function(req, res) {
   const id = req.query.id;
 
-  if (req.query.id == null || req.query.sender == null || req.query.receiver == null || req.query.msg == null || req.query.musicAvailable == null || req.query.musicLink == null){
-    res.send('Bro, string nya gak lengkap')
+  if (req.query.id == null){
+    res.json({ status: "failed", message: "Please fill all the fields" });
   }  
   
-  addNote(`${id}`, `${req.query.sender}`, `${req.query.receiver}`, `${req.query.msg}`, `${req.query.musicAvailable}`, `${req.query.musicLink}`)
+
+  setFirstNote(`${id}`)
   .then(() => { res.send('done') })
 })
+
+//note new post handler
+app.post("/notes/new", function(req, res) {
+  const data = NoteCount.find().exec();
+  const id = data.next;
+
+  if (req.body.sender == null || req.body.receiver == null || req.body.msg == null || req.body.musicAvailable == null || req.body.musicLink == null){
+    res.json({ status: "failed", message: "Please fill all the fields" });
+  }  
+  
+
+  addNote(`${id}`, `${req.body.sender}`, `${req.body.receiver}`, `${req.body.msg}`, `${req.body.musicAvailable}`, `${req.body.musicLink}`)
+  .then(() => { 
+    setCurrentNote()
+
+    res.json({ status: "success", message: "Yeah, its success, what did you expect with the message?", id: `${id}`}) 
+  })
+})
+
+//note new page
+app.get("/notes/new", function(req, res) {
+  res.send(`
+    <!DOCTYPE html>
+  
+  <html data-theme="light" lang="en">
+    <head>
+      <link rel="icon" href="/logo.png" type="image/x-icon" />
+      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta name="sendanote" content="true" />
+  
+      <title>New Note! - Send a Note</title>
+  
+      <link rel="stylesheet" href="/style.css" />
+      <link href="https://cdn.jsdelivr.net/npm/daisyui@2.15.3/dist/full.css" rel="stylesheet" type="text/css" />
+      <script src="https://cdn.tailwindcss.com"></script>
+      <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+      <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+      <script src="https://kit.fontawesome.com/c2165b4022.js" crossorigin="anonymous"></script>
+    </head>
+  
+  
+  
+  <body>
+  
+  <div class="navbar bg-base-100 sticky top-0 backdrop-blur-none shadow-sm">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+          </div>
+          <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><a href="/">Homepage</a></li>
+            <li><a href="/board">Board Notes</a></li>
+            <li><a href="/contribute">Contributions & Donations</a></li>
+            <li><a href="/report">Report & Request</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <a href="/" class="btn btn-ghost text-xl"><h1>Send a Note</h1></a>
+      </div>
+      <div class="navbar-end">
+          <div tabindex="0" role="button" class="text-current btn btn-ghost btn-circle">
+            <a href="/new"><i class="fa-solid fa-plus"></i></a>
+          </div>
+          </div>
+      </div>
+  </div>
+  
+  
+  
+  <div class="ml-[30px] mr-[30px] md:ml-[50px] md:mr-[50px]">
+    <section id="top">
+      <h1 class="text-5xl font-bold text-center mt-10">Send Your Note!</h1>
+      <p class="text-center mt-5">It's nice to see you're here to send your feelings!</p>
+      <br />
+      <fieldset class="fieldset">
+        <p id="errorMsg" style="display:none" class="validator-hint">
+          Please fill all of the required (*) fields
+        </p>
+        <br />
+        <legend class="fieldset-legend">Sender Name</legend>
+        <input id="sender" type="text" class="input" placeholder="Budi Tarmiji" />
+        <p class="fieldset-label">Optional</p>
+        <br />
+        <legend class="fieldset-legend">Recipient*</legend>
+        <input id="recipient" type="text" class="input" placeholder="Maimunah" required />
+        <legend class="fieldset-legend">Message*</legend>
+        <input id="msg" type="text" class="input" placeholder="Maimunah" required />
+        <legend class="fieldset-legend">Spotify Link</legend>
+        <input id="link" type="text" class="input" placeholder="https://open.spotify.com/track/0puyuBqmptiuq0K9ecvdW8?si=cAcc4N9LTGWkwNnSOtFlOg&context=spotify%3Aalbum%3A4bhftzHgTYXc9xy27QsryO" required />
+        <p class="fieldset-label">Leave it blank if you doesn't want to add a music</p>
+        <br />
+        <button class="btn btn-outline">Default</button>
+      </fieldset>
+      </div>
+  </section>
+  
+  <br />
+  
+  </body>
+  
+  <footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+  
+    <aside>
+  
+      <p>Copyright © 2025 - All right reserved by Always Lazy To Code | <a class="link link-primary" href="https://github.com/itshaiheree/sendanote">Star Us on Github</a></p>
+  
+    </aside>
+  
+  </footer>
+
+  <script>
+    var x = document.getElementById("sender").value;
+    var y = document.getElementById("recipient").value;
+    var z = document.getElementById("msg").value;
+    var a = document.getElementById("link").value;
+
+    if (x == "" || y == "" || z == "") {
+      document.getElementById("errorMsg").innerHTML = "Please fill all the fields";
+      document.getElementById("errorMsg").style.display = "block";
+    } else {
+     if (a == ""){
+      $(document).ready(function(){
+        $("button").click(function(){
+          $.post("demo_test_post.asp",
+            {
+              sender: x,
+              receiver: y,
+              msg: z,
+              musicAvailable: "hidden",
+              musicLink: "notAvailable"
+            },
+            function(data,status){
+              let nyum = JSON.parse(data);
+
+              location.replace('https://sendanote.mhai.my.id' + "/notes/" + nyum.id);
+          });
+        });
+      });
+      } else {
+        $(document).ready(function(){
+        $("button").click(function(){
+          $.post("demo_test_post.asp",
+            {
+              sender: x,
+              receiver: y,
+              msg: z,
+              musicAvailable: " ",
+              musicLink: a
+            },
+            function(data,status){
+              let nyum = JSON.parse(data);
+
+              location.replace('https://sendanote.mhai.my.id' + "/notes/" + nyum.id);
+          });
+        });
+      });
+    }
+  </script>
+    `)
+})
+
 // notes render
 app.get("/notes/:id", function(req, res) {
   const id = req.params.id;
@@ -208,8 +416,6 @@ app.get("/notes/:id", function(req, res) {
   (async () => {
   try {
   let data = await Note.findOne({ id: id }).exec();
-  
-console.log(data);
 
   const senderName = data.senderName;
   const senderTarget = data.receiver;
@@ -222,13 +428,13 @@ console.log(data);
 <html data-theme="light" lang="en">
 
   <head>
-    <link rel="icon" href="./logo.png" type="image/x-icon" />
+    <link rel="icon" href="/logo.png" type="image/x-icon" />
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="mazen" content="true" />
     <title>${senderName}'s Notes - SendANotes</title>
 
-    <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@2.15.3/dist/full.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
@@ -270,9 +476,9 @@ console.log(data);
     <div class="max-w-2xl mx-auto py-5 px-4">
     <div class="rounded-xl bg-[#E1C16E] w-full">
         <div class="card-body text-black">
-            <h2 class="text-xl font-bold">from: ${senderName}</h2>
-            <h2 class="text-xl font-bold">to: ${senderTarget}</h2>
-            <p class="text-l mt-3">${senderMsg}</p>
+            <a class="text-xl font-bold">from: </a><h2>${senderName}</h2>
+            <a class="text-xl font-bold">to: </a><h2>${senderTarget}</h2>
+            <h2 class="text-l mt-3">${senderMsg}</h2>
                 <div class="${senderSongVisibility} flex mt-4">
                 <div id="spotify-embed-iframe"></div>
                 </div>
@@ -328,28 +534,8 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
 
   const userAgent = navigator.userAgent;
 
-if (/Mobi|Android/i.test(userAgent)) {
-    
   const options = {
       width: 250,
-      height: 100,
-      uri: '${senderSong}'
-    };
-
-  const callback = (EmbedController) => {};
-  IFrameAPI.createController(element, options, callback);
-  } else if (/Tablet|iPad/i.test(userAgent)) {
-  const options = {
-      width: 400,
-      height: 100,
-      uri: '${senderSong}'
-    };
-
-  const callback = (EmbedController) => {};
-  IFrameAPI.createController(element, options, callback);
-  } else {
-  const options = {
-      width: 900,
       height: 100,
       uri: '${senderSong}'
     };
