79fb3935e98e69e32c6da5c9df075a0ef76902e9
Muhammad Haikal <king.haikalganteng@gmail.com>
2025-04-06 07:42:28 +0700

    feat: implement getNote function and update note retrieval logic

--- api/index.js
+++ api/index.js
@@ -19,6 +19,15 @@ const UsersSchema = new mongoose.Schema(
 
 let Note = mongoose.model("Note", UsersSchema);
 
+async function getNote(id) {
+  let ids = await Note.findOne({ id: id });
+  if (ids !== null) {
+    return ids;
+  } else {
+    return false;
+  }
+}
+
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -185,119 +194,9 @@ app.get("/notes/:id", function(req, res) {
 
   (async () => {
   try {
-  let data = Note.findOne({ id: `${id}` });
-
-  if (data.id === "undefined"){
-    res.send(`
-      <!DOCTYPE html>
-    <html data-theme="light" lang="en">
-    
-      <head>
-        <link rel="icon" href="./logo.png" type="image/x-icon" />
-        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-        <meta name="mazen" content="true" />
-        <title>The Note Doesn't Available - SendANotes</title>
-    
-        <link rel="stylesheet" href="./style.css" />
-        <link href="https://cdn.jsdelivr.net/npm/daisyui@2.15.3/dist/full.css" rel="stylesheet" type="text/css" />
-        <script src="https://cdn.tailwindcss.com"></script>
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <script src="https://kit.fontawesome.com/c2165b4022.js" crossorigin="anonymous"></script>
-        <script src="https://open.spotify.com/embed/iframe-api/v1" async></script>
-      </head>
-    
-    <body>
-    
-    <div class="navbar bg-base-100 sticky top-0 backdrop-blur-none shadow-sm">
-        <div class="navbar-start">
-          <div class="dropdown">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
-            </div>
-            <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li><a href="/">Homepage</a></li>
-              <li><a href="/board">Board Notes</a></li>
-              <li><a href="/contribute">Contributions & Donations</a></li>
-              <li><a href="/report">Report & Request</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="navbar-center">
-          <a href="/" class="btn btn-ghost text-xl"><h1>Send a Note</h1></a>
-        </div>
-        <div class="navbar-end">
-            <div tabindex="0" role="button" class="text-current btn btn-ghost btn-circle">
-              <a href="/new"><i class="fa-solid fa-plus"></i></a>
-            </div>
-            </div>
-        </div>
-    </div>
-    
-    
-    <div class="ml-[30px] mr-[30px] md:ml-[50px] md:mr-[50px]">
-        <div class="max-w-2xl mx-auto py-5 px-4">
-        <div class="rounded-xl bg-[#E1C16E] w-full">
-            <div class="card-body text-black">
-                <h2 class="text-xl font-bold">The note you're looking for doesn't available</h2>
-                <p class="text-l mt-3">The ${id} note doesn't available, try to look another note bellow!</p>
-            </div>
-          </div>
-    
-          <div class="">
-            <h2 class="text-3xl font-bold mt-10">Punya pesan? #NitipPesan aja!</h2>
-             <div class="grid grid-cols-1 gap-y-3 gap-x-6 sm:grid-cols-2 xl:gap-x-8 mt-4">
-                <a button class="btn btn-warning w-auto">Buat Pesanmu</a>
-                <a class="btn btn-active btn-error w-auto">Lihat Semua Pesan Yang Ada</a>
-            </div>
-          </div>
-    
-          <h2 class="text-3xl font-bold mt-10">Other Notes:</h2>
-          <div class="max-w-2xl mx-auto py-5 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8 text-black">
-            <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                <div class="rounded-xl bg-[#E1C16E] w-full">
-                  <div class="card-body">
-                    <h2 class="card-title">Papan Pengakuan</h2>
-                    <p>Papan Yang Isinya Segala Macam Pengakuan Warga Bumi</p>
-                  </div>
-                </div>
-              
-              <div class="rounded-xl bg-[#E1C16E] w-full">
-                <div class="card-body">
-                  <h2 class="card-title">Papan Pengakuan</h2>
-                  <p>Papan Yang Isinya Segala Macam Pengakuan Warga Bumi</p>
-                </div>
-              </div>
-      
-              <div class="rounded-xl bg-[#E1C16E] w-full">
-                <div class="card-body">
-                  <h2 class="card-title">Papan Pengakuan</h2>
-                  <p>Papan Yang Isinya Segala Macam Pengakuan Warga Bumi</p>
-                </div>
-              </div>
-      
-              <div class="rounded-xl bg-[#E1C16E] w-full">
-                <div class="card-body">
-                  <h2 class="card-title">Papan Pengakuan</h2>
-                  <p>Papan Yang Isinya Segala Macam Pengakuan Warga Bumi</p>
-                </div>
-              </div>
-            </div>
-            </div>
-    </div>
-    <br />
-    </body>
-    <script>
-    </script>
-    <footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-      <aside>
-        <p>Copyright © 2025 - All right reserved by Always Lazy To Code | <a class="link link-primary" href="https://github.com/itshaiheree/sendanote">Star Us on Github</a></p>
-      </aside>
-    </footer>
-      `)
-  }
+  let data = getNote(id);
+  
+console.log(data);
 
   const senderName = data.senderName;
   const senderTarget = data.receiver;
